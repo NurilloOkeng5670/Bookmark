cf0f57328aeebedcff3742a2740031ab0e632be6
K_hay1tov <104296275+NurilloOkeng5670@users.noreply.github.com>
2022-07-02 09:16:20 +0300

    Add files via upload
    Bookmark figma

---
+++ main.js
@@ -0,0 +1,26 @@
+// let elForm = document.querySelector(".js-form");
+// let elInput = document.querySelector(".js-input");
+// let elResult = document.querySelector(".js-return");
+
+// elForm.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     let elInputText = elInput.value;
+
+//     if(elInputText < 15) {
+//         elResult.textContent = `15 dan ko'proq son kiriting`;
+//     }else if(elInput == 'string') {
+//         elResult.textContent = `Harf kiritmang`;
+
+//     }else{
+
+//         elResult.textContent = '';
+//     }
+
+//     elResult.textContent = `Harf kiritmang yoki 15 ta dan ko'proq harf kiriting `;
+//     console.log(elInputText);
+
+// });
+// elResult.textContent = `To'g'ri kiriting`;
+
+
+// console.log("Salom");
